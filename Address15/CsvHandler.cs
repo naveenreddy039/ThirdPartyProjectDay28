eefac1db8b963c3f7cbc1134659138bdde67c547
naveen <naveenreddy.k07@gmail.com>
2022-01-13 11:32:01 +0530

    addreessBook UC15

---
+++ Address15/CsvHandler.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+
+namespace Address15
+{
+    public class CsvHandler
+    {
+        public static void ImplementCSVDataHandling()
+        {
+            string importPath = @"C:\Users\hp\source\repos\Address15\Address15\Demo.csv";
+            string exportpath = @"C:\Users\hp\source\repos\Address15\Address15\Demo1.json";
+
+            using (var reader = new StreamReader(importPath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<addressData>().ToList();
+                Console.WriteLine("Read Data Successfully from address.csv");
+                foreach (addressData addressData in records)
+                {
+                    Console.WriteLine("" + addressData.FirstName);
+                    Console.WriteLine("" + addressData.LastName);
+
+                    Console.WriteLine("" + addressData.Address);
+
+                    Console.WriteLine("" + addressData.City);
+
+                    Console.WriteLine("" + addressData.State);
+                    Console.WriteLine("" + addressData.Code);
+                    Console.WriteLine("\n");
+
+
+                }
+
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(exportpath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, records);
+                }
+            }
+        }
+    }
+}
---
+++ Address15/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Address15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read data from CSV $ Write data in CSV");
+            CsvHandler.ImplementCSVDataHandling();
+        }
+    }
+}
